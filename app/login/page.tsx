eb743b507b24ccd67ac8afac161cbe8da7b5e0f9
Berk Kendirlioğlu <71317893+berkkendirlioglu@users.noreply.github.com>
2024-11-27 15:22:09 +0300

    change

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import LoginPageBackground from '@/components/login-page-bg';
 import { getAllMovies } from '@/services/FetchProcess'
 import { randomInt } from 'crypto';
